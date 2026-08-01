20a1179edcfde26639564212e0dcf57f4f5b0589
Aman Nikhar <amannikhar23@gmail.com>
2026-08-02 00:31:56 +0530

    Configure frontend for deployment

--- Frontend/algo-lab/vite.config.js
+++ Frontend/algo-lab/vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      // during dev, calls to /api/* are forwarded to your Express backend
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true
